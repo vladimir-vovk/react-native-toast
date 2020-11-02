0f9e2ec8b0ef49f9abbbdecd1256f1ac834b327e
Vladimir Vovk <vladimir@vovk.in>
2020-11-02 15:09:26 +0300

    fix: remove status bar

--- demo/App.tsx
+++ demo/App.tsx
@@ -1,4 +1,3 @@
-import { StatusBar } from 'expo-status-bar'
 import React from 'react'
 import { ToastProvider, Toast } from '@vladimir-vovk/react-native-toast'
 import Demo from './src/Demo'
@@ -6,7 +5,6 @@ import Demo from './src/Demo'
 export default function App() {
   return (
     <ToastProvider config={{ timeout: 2000 }}>
-      <StatusBar style="auto" />
       <Demo />
       <Toast />
     </ToastProvider>
